ae3f769f19a4c2da56e6b26c8a19255c9810b3b6
Vincent BRAY <develop@vincent-bray.com>
2023-10-18 10:34:44 +0200

    📦 NEW: Task Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Task;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,25 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        // Création entre 15 et 30 tâches aléatoirement
+        for ($t = 0; $t < mt_rand(15, 30); $t++) {
+
+            // Création d'un nouvel objet Task
+            $task = new Task;
+
+            // On nourrit l'objet Task
+            $task->setName($faker->sentence(6))
+                ->setDescription($faker->paragraph(3))
+                ->setCreatedAt(new \DateTimeImmutable()) // Attention les dates sont créées en fonction du réglage serveur
+                ->setDueAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('now', '6 months'))); // Même chose ici
+
+            // On fait persister les données
+            $manager->persist($task);
+
+        }
 
         $manager->flush();
     }
