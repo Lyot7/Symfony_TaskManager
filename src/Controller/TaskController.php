0518073bc27c4d70818d3d1864a24030f49fcf7c
Vincent BRAY <develop@vincent-bray.com>
2023-10-18 16:22:58 +0200

    📦 NEW: Delete Task Feature

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -41,11 +41,13 @@ class TaskController extends AbstractController
     }
 
     #[Route('/task/create', name: 'app_task_create')]
-    public function createTask(Request $request){
+    #[Route('/task/update/{id}', name: 'app_task_update', requirements: ['id' => "\d+"])]
+    public function task(Task $task = null, Request $request){
 
-        $task = new Task;
-
-        $task->setCreatedAt(new \DateTime());
+        if (!$task){
+            $task = new Task;
+            $task->setCreatedAt(new \DateTime());
+        }
 
         $form = $this->createForm(TaskType::class, $task, []);
 
@@ -53,11 +55,6 @@ class TaskController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             
-            // $task->setName($form['name']->getData())
-            //     ->setDescription($form['description']->getData())
-            //     ->setDueAt($form['dueAt']->getData())
-            //     ->setTag($form['tag']->getData());
-
             $this->manager->persist($task);
             $this->manager->flush();
 
@@ -69,25 +66,13 @@ class TaskController extends AbstractController
         ]);
     }
 
-    #[Route('/task/update/{id}', name: 'app_task_update', requirements: ['id' => "\d+"])]
-    public function updateTask(int $id, Request $request){
+    #[Route('/task/delete/{id}', name: 'app_task_delete', requirements: ['id' => "\d+"])]
+    public function deleteTask(Task $task): Response
+    {
+        $this->manager->remove($task);
+        $this->manager->flush();
 
-        $task = $this->taskRepository->find($id);
-
-        $form = $this->createForm(TaskType::class, $task, []);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $this->manager->persist($task);
-            $this->manager->flush();
-
-            return $this->redirectToRoute('app_task');
-        }
-
-        return $this->render('task/create.html.twig', [
-            'form' => $form->createView()
-        ]);
+        return $this->redirectToRoute('app_task');
     }
+
 }
